ab41cf2db43470f8bd37c387418e4c4db999ee21
Nícolas Auersvalt <nicolasauersvalt@gmail.com>
2024-09-05 00:28:01 -0300

    Add files via upload

---
+++ header.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.stats import norm
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import confusion_matrix, accuracy_score
+from sklearn.model_selection import cross_val_score
+from scipy.spatial.distance import pdist
+from scipy.cluster.hierarchy import linkage, fcluster, dendrogram
+from sklearn.datasets import load_iris
+from sklearn.cluster import KMeans
+import seaborn as sns
+import pandas as pd
